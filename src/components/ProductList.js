2ae0913f23cf296c0e04e2831b3c2c677d6717aa
Raghav Malhotra <malhotraraghav699@gmail.com>
2024-01-25 17:46:00 -0500

    added various hooks and used a mock db json-server

---
+++ src/components/ProductList.js
@@ -0,0 +1,46 @@
+import React, { useCallback, useEffect, useState } from 'react'
+
+function ProductList() {
+  const [products, setProducts] = useState([])
+  const [url, setUrl] = useState('http://localhost:8000/products/')
+
+  const fetchProducts = useCallback(async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setProducts(data)
+  }, [url])
+
+  useEffect(() => {
+    fetchProducts()
+  }, [fetchProducts])
+
+  // console.log(products)
+  return (
+    <section>
+      <div className='filter'>
+        <button onClick={() => setUrl('http://localhost:8000/products')}>
+          all
+        </button>
+        <button
+          onClick={() => setUrl('http://localhost:8000/products?in_stock=1')}
+        >
+          instock
+        </button>
+      </div>
+      {products.map((product) => (
+        <div className='card' key={product.id}>
+          <p className='id'>{product.id}</p>
+          <p className='name'> {product.name}</p>
+          <p className='info'>
+            <span>${product.price}</span>
+            <span className={product.in_stock ? 'instock' : 'unavailable'}>
+              {product.in_stock ? 'in stock' : 'unavailable'}
+            </span>
+          </p>
+        </div>
+      ))}
+    </section>
+  )
+}
+
+export default ProductList
